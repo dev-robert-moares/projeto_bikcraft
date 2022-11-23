87c254db84265124b81f41f6b24f47c4401bee67
Robert Moraes <robertmoraes13r@gmail.com>
2022-11-23 10:55:04 -0300

    modificacao da pagina de orcamento para que quando comprasse a bicicleta, ela já estivesse selecionada

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Ativar Links do Menu
 const links = document.querySelectorAll('.header-menu a')
 
 function ativarLink(link) {
@@ -11,3 +12,16 @@ function ativarLink(link) {
 
 links.forEach(ativarLink)
 
+// Ativar itens do orcamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro)
+  if (elemento) {
+    elemento.checked = true;
+  }
+  
+}
+
+parametros.forEach(ativarProduto)
