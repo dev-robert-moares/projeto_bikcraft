1fe820eb6fb4f743b4c0023ea0fd0630e37d0fc4
Robert Moraes <robertmoraes13r@gmail.com>
2022-11-23 11:39:15 -0300

    deixando a seção de perguntas e respostas interativa

--- js/script.js
+++ js/script.js
@@ -1,27 +1,46 @@
 //Ativar Links do Menu
-const links = document.querySelectorAll('.header-menu a')
+const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
   const url = location.href;
   const href = link.href;
 
   if (url.includes(href)) {
-    link.classList.add('ativo')
+    link.classList.add("ativo");
   }
 }
 
-links.forEach(ativarLink)
+links.forEach(ativarLink);
 
 // Ativar itens do orcamento
 
 const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
-  const elemento = document.getElementById(parametro)
+  const elemento = document.getElementById(parametro);
   if (elemento) {
     elemento.checked = true;
   }
-  
 }
 
-parametros.forEach(ativarProduto)
+parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
